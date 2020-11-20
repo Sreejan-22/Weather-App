557745223dfc501e21ed5f0acf8c56b6ba33d056
Sreejan Chaudhury <64251207+Sreejan-22@users.noreply.github.com>
2020-11-20 18:43:24 +0530

    Create forecast.js

---
+++ forecast.js
@@ -0,0 +1,19 @@
+const key = "0XVG4b3GnxAxPqjzj0NnaMGBuJnd2itz"; 
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
